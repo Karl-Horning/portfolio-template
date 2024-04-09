c655dbb170cb3fefadb9f14b39ff35b4187c48bb
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-04-10 00:15:05 +0100

    Add example headers to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,67 @@
-import { Spinner } from "@nextui-org/react";
-
 export default function Home() {
     return (
         <main>
-            <h1>Portfolio Template</h1>
-            <Spinner />
+            {/* Hero Section */}
+            <h1>Let&apos;s build something great together</h1>
+
+            {/* About Section */}
+            <h2>About Me</h2>
+
+            <h3>Who Am I?</h3>
+
+            <h3>Expert In</h3>
+
+            <h3>Education</h3>
+
+            <h3>Experience</h3>
+
+            {/* Skills Section */}
+            <h2>What I Do</h2>
+
+            <h3>Web Design &amp; UI</h3>
+
+            <h3>User Flow &amp; Wireframes</h3>
+
+            <h3>Visual Design</h3>
+
+            <h3>Mobile App Design</h3>
+
+            <h3>Logo Design</h3>
+
+            <h3>Print Design</h3>
+
+            {/* Portfolio Section */}
+            <h2>Awesome Work</h2>
+
+            {/* Testimonials Section */}
+            <h2>Why My Clients Say</h2>
+
+            <h3>Why Choose Me?</h3>
+
+            <h3>Client&apos;s Feedback</h3>
+
+            {/* Blog Section */}
+            <h2>Helpful Writing</h2>
+
+            {/* Stats Section */}
+            <h2>Useful Stats</h2>
+
+            <h3>2,500 Projects Completed</h3>
+
+            <h3>400 Happy Clients</h3>
+
+            <h3>98K Lines of Code</h3>
+
+            <h3>78K Cups of Coffee</h3>
+
+            {/* Contact Section */}
+            <h2>Contact Me</h2>
+
+            {/* Subscribe Section */}
+            <h2>Subscribe to Get My Newsletter</h2>
+
+            {/* Footer Section */}
+            <p>Copyright &copy; 2024 Karl Horning. All Rights Reserved.</p>
         </main>
     );
 }
