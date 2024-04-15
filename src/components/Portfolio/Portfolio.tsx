e60aacd62485e5c6f43b9fe8d898af8886c0c571
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-04-15 19:54:42 +0100

    Update portfolio component to use other portfolio components and add JSDoc

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,12 +1,18 @@
 import Header from "@/components/Header/Header";
 import PortfolioFilter from "@/components/Portfolio/PortfolioFilter";
+import PortfolioGrid from "@/components/Portfolio/PortfolioGrid";
 
+/**
+ * Portfolio component displays a section containing a header, portfolio filter, and portfolio grid.
+ * @returns {JSX.Element} Portfolio component.
+ */
 export default function Portfolio() {
     return (
         <section id="portfolio" className=" px-10 py-40">
             <div className="container mx-auto">
                 <Header title="Awesome Work" />
                 <PortfolioFilter />
+                <PortfolioGrid />
             </div>
         </section>
     );
