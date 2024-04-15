7a4dc978aebc7d895fed517fab59a28ca48715a5
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-04-15 08:56:56 +0100

    Add optional CSS prop to outline button

--- src/components/Buttons/OutlineButton.tsx
+++ src/components/Buttons/OutlineButton.tsx
@@ -3,9 +3,11 @@ import { Button } from "@nextui-org/react";
 /**
  * Props for the OutlineButton component.
  * @prop {string} title - The title text displayed on the button.
+ * @prop {string} className - Any additional CSS classes to be displayed on the button.
  */
 interface OutlineButtonProps {
     title: string;
+    className?: string;
 }
 
 /**
@@ -13,13 +15,13 @@ interface OutlineButtonProps {
  * @param {OutlineButtonProps} props - Props for the OutlineButton component.
  * @returns {JSX.Element} JSX representing the OutlineButton component.
  */
-export default function OutlineButton({ title }: Readonly<OutlineButtonProps>) {
+export default function OutlineButton({ title, className }: Readonly<OutlineButtonProps>) {
     return (
         <Button
             size="lg"
             radius="none"
             variant="ghost"
-            className="border-black uppercase text-black"
+            className={`border-black uppercase text-black ${className}`}
         >
             {title}
         </Button>
