67d343e6332ce3adb581478e314602bf13042101
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-04-16 22:53:39 +0100

    Add footer component and styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,21 @@
+/**
+ * Footer component displays the copyright information in the footer section.
+ * @returns {JSX.Element} JSX element representing the footer.
+ */
 export default function Footer() {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+
     return (
-        <section id="newsletter" className="container mx-auto my-10">
-            <div className="my-20">
-                <p className="text-center">Copyright &copy; 2024 Karl Horning. All Rights Reserved.</p>
-            </div>
+        <section
+            id="newsletter"
+            className="border-t-2 border-white bg-black px-10 py-10 text-white"
+        >
+            <p className="text-center">
+                Copyright &copy;{" "}
+                {currentYear === 2024 ? "2024" : `2024 - ${currentYear}`} Karl
+                Horning. All Rights Reserved.
+            </p>
         </section>
     );
 }
