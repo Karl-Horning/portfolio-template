41ec2a9dc316813f07a5558b4a7c7a31863b37b1
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-04-16 23:18:39 +0100

    Add newsletter component

--- src/components/Newsletter/Newsletter.tsx
+++ src/components/Newsletter/Newsletter.tsx
@@ -1,10 +1,39 @@
 import Header from "@/components/Header/Header";
+import { Input } from "@nextui-org/react";
+import { OutlineButton } from "@/components/Buttons";
 
+/**
+ * Newsletter component.
+ * @returns {JSX.Element} JSX element representing the Newsletter component.
+ */
 export default function Newsletter() {
     return (
         <section id="newsletter" className="bg-black px-10 py-40 text-white">
             <div className="container mx-auto">
-                <Header title="Newsletter" whiteHeader={true} />
+                <Header title="Newsletter" whiteHeader />
+
+                <form action="" className="items-center text-center">
+                    <p className="mb-10 text-xl font-bold uppercase">
+                        Subscribe to get my newsletter
+                    </p>
+
+                    <div className="mb-10 flex items-center justify-center">
+                        <Input
+                            isClearable
+                            isRequired
+                            className="max-w-lg bg-white text-black"
+                            label="Email"
+                            radius="none"
+                            type="email"
+                            variant="bordered"
+                        />
+                    </div>
+
+                    <OutlineButton
+                        title="Subscribe"
+                        className="border-white text-white"
+                    />
+                </form>
             </div>
         </section>
     );
